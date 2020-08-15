ba821584da1a5523106403201beda810320df163
SWZ <contact@swz.works>
2020-08-15 14:07:02 +0200

    Make button disabled while loading sound

--- src/script.js
+++ src/script.js
@@ -1,6 +1,21 @@
+async function sleep(t) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, t);
+  });
+}
+
+let currentSound;
+
 document.querySelector("button.submitButton").onclick = async () => {
   let link = document.querySelector("input.linkInput").value;
 
-  let audio = new Audio(link);
-  audio.play();
+  if (currentSound) {
+    currentSound.pause();
+  }
+
+  document.querySelector("button.submitButton").disabled = true;
+  currentSound = new Audio(link);
+  await sleep(500);
+  document.querySelector("button.submitButton").disabled = false;
+  currentSound.play();
 };
