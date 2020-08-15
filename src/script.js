585b9f7d9cb8cff56946a58a183f2a2b03b4efb6
SWZ <contact@swz.works>
2020-08-15 14:17:55 +0200

    Added stop button

--- src/script.js
+++ src/script.js
@@ -6,16 +6,21 @@ async function sleep(t) {
 
 let currentSound;
 
-document.querySelector("button.submitButton").onclick = async () => {
+document.querySelector("button.playButton").onclick = async () => {
   let link = document.querySelector("input.linkInput").value;
 
   if (currentSound) {
     currentSound.pause();
   }
 
-  document.querySelector("button.submitButton").disabled = true;
+  document.querySelector("button.playButton").disabled = true;
   currentSound = new Audio(link);
   await sleep(500);
-  document.querySelector("button.submitButton").disabled = false;
+  document.querySelector("button.playButton").disabled = false;
   currentSound.play();
+};
+document.querySelector("button.stopButton").onclick = async () => {
+  if (currentSound) {
+    currentSound.pause();
+  }
 };
