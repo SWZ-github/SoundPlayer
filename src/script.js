fb88124ec53e5bdb4dbc379ee74f9601088edfa0
SWZ <contact@swz.works>
2020-08-16 13:35:40 +0200

    Added loop button and functionality

--- src/script.js
+++ src/script.js
@@ -5,22 +5,42 @@ async function sleep(t) {
 }
 
 let currentSound;
+let looping = false;
+
+function killSound() {
+  if (!currentSound) return;
+  currentSound.pause();
+  currentSound = undefined;
+}
 
 document.querySelector("button.playButton").onclick = async () => {
   let link = document.querySelector("input.linkInput").value;
 
   if (currentSound) {
-    currentSound.pause();
+    killSound();
   }
 
   document.querySelector("button.playButton").disabled = true;
   currentSound = new Audio(link);
+
   await sleep(500);
+
   document.querySelector("button.playButton").disabled = false;
+
   currentSound.play();
+
+  currentSound.addEventListener("ended", (ev) => {
+    if (looping) {
+      currentSound.play();
+    }
+  });
 };
-document.querySelector("button.stopButton").onclick = async () => {
-  if (currentSound) {
-    currentSound.pause();
+document.querySelector("button.stopButton").onclick = killSound;
+document.querySelector("button.loopButton").onclick = async () => {
+  looping = !looping;
+  if (looping) {
+    document.querySelector("button.loopButton").classList.add("active");
+  } else {
+    document.querySelector("button.loopButton").classList.remove("active");
   }
 };
